c3cf942248e74ab94c89428766475d6098215366
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-05-19 15:58:53 +0300

    Refactor App component to improve data fetching with abort controller; implement debouncing for search input and enhance error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import Container from '@mui/material/Container';
 import NewsFeed from './components/NewsFeed';
 import NewsHeader from './components/NewsHeader';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo, useRef } from 'react';
+import { debounce } from 'lodash';
 
 function App() {
   const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
@@ -9,26 +10,48 @@ function App() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  async function loadData(inputQuery) {
-    const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?q=${inputQuery}&country=us&apiKey=${API_KEY}`
-    );
+  const abortControllerRef = useRef(null);
 
-    const data = await response.json();
-    console.log(data);
-    return data.articles.map((article) => {
-      const { title, description, author, publishedAt, urlToImage } = article;
-      return {
-        title,
-        description,
-        author,
-        publishedAt,
-        image: urlToImage,
-      };
-    });
+  async function loadData(inputQuery) {
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+
+    abortControllerRef.current = new AbortController();
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?q=${inputQuery}&country=us&apiKey=${API_KEY}`,
+        { signal: abortControllerRef.current.signal }
+      );
+
+      const data = await response.json();
+
+      return data.articles.map((article) => {
+        const { title, description, author, publishedAt, urlToImage } = article;
+        return {
+          title,
+          description,
+          author,
+          publishedAt,
+          image: urlToImage,
+        };
+      });
+    } catch (error) {
+      if (error.name === 'AbortError') {
+        return null;
+      }
+      console.error('API Error:', error);
+      return [];
+    }
   }
 
-  console.log('App reevaluated');
+  const debouncedLoadData = debounce((newQuery) => {
+    setLoading(true);
+    loadData(newQuery).then((newData) => {
+      setArticles(newData);
+      setLoading(false);
+    });
+  }, 500);
 
   useEffect(() => {
     setLoading(true);
@@ -39,11 +62,7 @@ function App() {
   }, []);
 
   const handleSearchChange = (newQuery) => {
-    setLoading(true);
-    loadData(newQuery).then((newData) => {
-      setArticles(newData);
-      setLoading(false);
-    });
+    debouncedLoadData(newQuery);
   };
 
   return (
